a8a44e5bc1fbe0836f017ac45aea4a1b29c3d3c4
Даниил Тюкин <tyukin.01@mail.ru>
2024-02-28 18:02:39 +0300

    Нужно больше функций

--- js/function.js
+++ js/function.js
@@ -4,31 +4,31 @@ function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
-checkStringLength("проверяемая строка", 10);
-checkStringLength("проверяемая строка", 18);
-checkStringLength("проверяемая строка", 20);
+checkStringLength('проверяемая строка', 10);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 20);
 
 // 2. Функция для проверки, является ли строка палиндромом.
 
 function isPalindrome(str) {
-  const normalizedStr = str.replaceAll(" ", "").toLowerCase();
-  let reversedStr = "";
+  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
   for (let i = normalizedStr.length - 1; i >= 0; i--) {
     reversedStr += normalizedStr[i];
   }
   return normalizedStr === reversedStr;
 }
 
-isPalindrome("топот");
-isPalindrome("ДовОд");
-isPalindrome("Кекс");
-isPalindrome("Лёша на полке клопа нашёл ");
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
+isPalindrome('Лёша на полке клопа нашёл ');
 
 // 3. Дополнительное задание.
 
 const exctractNumber = (arg) => {
   const string = arg.toString();
-  let result = "";
+  let result = '';
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string[i], 10))) {
       result += string[i];
@@ -38,8 +38,8 @@ const exctractNumber = (arg) => {
   return parseInt(result, 10);
 };
 
-exctractNumber("2023 год");
-exctractNumber("ECMAScript 2022");
-exctractNumber("1 кефир, 0.5 батона");
-exctractNumber("агент 007");
-exctractNumber("а я томат");
+exctractNumber('2023 год');
+exctractNumber('ECMAScript 2022');
+exctractNumber('1 кефир, 0.5 батона');
+exctractNumber('агент 007');
+exctractNumber('а я томат');
