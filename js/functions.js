4efae851c25e76db496e6f7bd7f3675fac91df31
Даниил Тюкин <tyukin.01@mail.ru>
2024-03-26 12:28:32 +0300

    исправил ошибку

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,20 @@
-const checkMeetingTime = (
-  timeStartWork,
-  timeEndWork,
-  timeStartMeeting,
-  durationMeeting
-) => {
-  const timesArray = [timeStartWork, timeEndWork, timeStartMeeting];
-  const timeInMinutesArray = [];
-  for (let i = 0; i < 3; i++) {
-    const partTime = timesArray[i].split(':');
-    const timeInMinutes = Number(partTime[0]) * 60 + Number(partTime[1]);
-    timeInMinutesArray.push(timeInMinutes);
-  }
-  return (
-    timeInMinutesArray[2] >= timeInMinutesArray[0] &&
-    timeInMinutesArray[2] + durationMeeting <= timeInMinutesArray[1]
-  );
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-checkMeetingTime('08:00', '17:30', '14:00', 90); // true
-checkMeetingTime('8:0', '10:0', '8:0', 120); // true
-checkMeetingTime('08:00', '14:30', '14:00', 90); // false
-checkMeetingTime('14:00', '17:30', '08:0', 90); // false
-checkMeetingTime('8:00', '17:30', '08:00', 900); // false
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+function createIdGenerator() {
+  let lastGeneratedID = 0;
+
+  return () => {
+    lastGeneratedID += 1;
+    return lastGeneratedID;
+  };
+}
+
+export { getRandomInteger, getRandomArrayElement, createIdGenerator };
