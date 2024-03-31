f60622a029893c6f6f51661c422680b1a7344841
Даниил Тюкин <tyukin.01@mail.ru>
2024-04-01 01:50:29 +0300

    небольшая правка

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { similarPhoto } from './data.js';
 import { drawSimilarPhoto } from './picture.js';
 import './big-picture.js';
-import './form.js';
+import './render-form.js';
 
 const photos = similarPhoto();
 
